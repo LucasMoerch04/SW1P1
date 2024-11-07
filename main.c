94db9421bd5ad452015e2e56fd8e7f6a96a34b0f
Mohamed <SE37GN@student.aau.dk>
2024-11-07 13:56:14 +0100

    skill

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include "retrieveAPI.h"
 
 int main(void){
     get_coordinates();
 
-    printf("hello, world\n");
+    printf("Mohamed"\n");
 
     printf("%lf", lat);
     printf("%lf", lon);

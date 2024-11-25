95c9069914d8746285b7170de81ab8a3bff6d41b
Lucas Moerch <lucasmoerch04@icloud.com>
2024-11-25 10:11:29 +0100

    test

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "retrieveAPI.h"
 #include "applicantStruct.h"
+#include "co2.h"
 
 int main(void){
 
 
     get_coordinates(2650, "Bymuren");
     default_list();
+    printf("CO2: %lf\n", CalculateEmissions(200, "Car"));
 
     printf("hello, world\n");
 
